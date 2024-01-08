ef28b06a42d7eeb50a841b3f7b2a391c22c8926b
KStawiasz <krystianstawiasz@gmail.com>
2024-01-08 16:29:26 +0100

    updt

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -5,43 +5,125 @@ Scenariusz nr 1
 */
 
 #include <iostream>
-#include <SFML/Graphics.hpp>
 
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+
+//Classes
+class Game
+{
+private:
+	//Variables
+	//Window
+	sf::RenderWindow* window;
+	sf::VideoMode videoMode;
+	sf::Event ev;
+
+	//Private functions
+	void initVariables();
+	void initWindow();
+
+public:
+	//Contstructors / Destructors
+	Game();
+	virtual ~Game();
+
+	//Accessors
+	const bool running() const;
+
+	//Functions
+	void pollEvents();
+	void update();
+	void render();
+};
+
+
+//Contstructors / Destructors
+Game::Game()
+{
+	this->initVariables();
+	this->initWindow();
+}
+
+Game::~Game()
+{
+	delete this->window;
+}
+
+//Accessors
+const bool Game::running() const
+{
+	return this->window->isOpen();
+}
+
+//Functions
+void Game::pollEvents()
+{
+	//zdarzenia polling
+	while (this->window->pollEvent(this->ev))
+	{
+		switch (this->ev.type)
+		{
+		case sf::Event::Closed:
+			this->window->close();
+			break;
+		case sf::Event::KeyPressed:
+			if (this->ev.key.code == sf::Keyboard::Escape)
+				this->window->close();
+			break;
+		}
+	}
+}
+
+void Game::update()
+{
+	this->pollEvents();
+}
+
+void Game::render()
+{
+	/*
+	- clear old frame
+	- render objects
+	- display frame in window
+	Renderuje gierke
+	*/
+	this->window->clear();
+
+	this->window->display();
+}
+
+void::Game::initVariables()
+{
+	this->window = nullptr;
+}
+
+void::Game::initWindow()
+{
+	this->videoMode.height = 600;
+	this->videoMode.width = 800;
+
+	this->window = new sf::RenderWindow(this->videoMode, "Space Ship", sf::Style::Titlebar | sf::Style::Close);
+}
 
 int main()
 {
 
-	//Okno 
-	sf::RenderWindow window(sf::VideoMode(640, 480), "Space Ship", sf::Style::Titlebar | sf::Style::Close);
-	sf::Event ev;
+	//Init Game engine
+	Game game;
 
-	//Petla gry
-	while (window.isOpen())
+	//Game loop
+	while (game.running())
 	{
-		//zdarzenia polling
-		while (window.pollEvent(ev))
-		{
-			switch (ev.type)
-			{
-			case sf::Event::Closed:
-				window.close();
-				break;
-
-			case sf::Event::KeyPressed:
-				if (ev.key.code == sf::Keyboard::Escape)
-					window.close();
-				break;
-			}
-		}
-
 		//Update
+		game.update();
 
 		//Render
-		window.clear(sf::Color::Magenta); //Clear old frame
-
-		//Draw your game 
-
-		window.display(); // Tell app that window is done drawing 
+		game.render();
 	}
 
 	//koniec gry
